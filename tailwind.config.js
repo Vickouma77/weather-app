f1e503d7584b1dc3cf6ac4ebe48438691995a86b
Vickouma77 <Vickprestone20@gmail.com>
2024-05-28 23:22:24 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#008a5c",
+        "weather-secondary": "#036242",
+      },
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
+  },
+  plugins: [],
+};
