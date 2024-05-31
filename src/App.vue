665840b4431cc7cb8613eb9e74314ab365804ff1
Vickouma77 <Vickprestone20@gmail.com>
2024-05-31 13:40:50 +0300

    Adding footer section

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,15 @@
       </Transition>
     </RouterView>
   </div>
+  <div>
+    <Footer />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import Navigation from "./components/Navigation.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <style>
